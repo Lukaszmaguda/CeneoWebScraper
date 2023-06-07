6a70f7dca9706574d1d5f204065c47a497f28098
Lukaszmaguda <rometzet@gmail.com>
2023-06-07 14:29:24 +0200

    2023-06-07 ŁM

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
 from app import app
+from flask import render_template
 
-@app.route("/name",defaults={'name':"Anonim"})
-@app.route("/name<name>")
-def name(name):
-    return f"Hello{name}!"
+@app.route("/")
+@app.route("/strona_glowna")
+def strona_glowna():
+    return render_template("strona_glowna.html")
+
+@app.route("/autor")
+def autor():
+    return render_template("autor.html")
+
+@app.route("/Ekstrakcja")
+def ekstrakcja():
+    return render_template("ekstrakcja_opinii.html")
+
+@app.route("/Lista_Produktow")
+def lista_poduktow():
+    return render_template("lista_produktow.html")
+
+@app.route("/Strona główa")
+def strona_glowna():
+    return render_template("strona_glowna.html")
